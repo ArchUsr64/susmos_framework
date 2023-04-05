3af298cc9d5f394a2773d1837065f08fbbbb057d
ArchUsr64 <anshulusr@gmail.com>
2023-04-05 16:22:07 +0530

    added graph builder class

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,72 @@
 use macroquad::prelude::*;
-pub const F: bool = false;
-pub const T: bool = true;
 
 struct Graph {
 	vertex: Vec<char>,
 	edges: Vec<(usize, usize)>,
 }
 
+struct GraphBuilder {
+	graph: Graph,
+}
+impl GraphBuilder {
+	pub fn new() -> Self {
+		Self {
+			graph: Graph {
+				vertex: Vec::new(),
+				edges: Vec::new(),
+			},
+		}
+	}
+	pub fn add_vertex(mut self, value: char) -> Self {
+		if !self.graph.vertex.contains(&value) {
+			self.graph.vertex.push(value);
+		}
+		self
+	}
+	pub fn add_edge(mut self, value1: char, value2: char) -> Self {
+		self = self.add_vertex(value1);
+		self = self.add_vertex(value2);
+		let find_index = |value| {
+			self.graph
+				.vertex
+				.iter()
+				.enumerate()
+				.find(|(index, char)| **char == value)
+				.map(|(index, _)| index)
+				.unwrap()
+		};
+		let index1 = find_index(value1);
+		let index2 = find_index(value2);
+		println!("{index1}, {index2}");
+		if !(self.graph.edges.contains(&(index1, index2))
+			|| self.graph.edges.contains(&(index2, index1)))
+		{
+			println!("Added: {value1}, {value2}");
+			self.graph.edges.push((index1, index2))
+		}
+		self
+	}
+	pub fn build(self) -> Graph {
+		self.graph
+	}
+}
+
 #[macroquad::main("BasicShapes")]
 async fn main() {
-	let g = Graph {
-		vertex: vec!['A', 'B', 'C', 'D', 'E', 'F'],
-		edges: vec![(0, 1), (2, 1), (4, 0), (4, 2), (3, 1), (5, 2)],
-	};
+	let g = GraphBuilder::new()
+		.add_vertex('A')
+		.add_vertex('B')
+		.add_vertex('C')
+		.add_vertex('D')
+		.add_vertex('E')
+		.add_vertex('F')
+		.add_edge('A', 'B')
+		.add_edge('C', 'B')
+		.add_edge('E', 'A')
+		.add_edge('E', 'C')
+		.add_edge('D', 'B')
+		.add_edge('F', 'C')
+		.build();
 	let positions = g
 		.vertex
 		.iter()
