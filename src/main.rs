870a04e9072323be863df24719b6785be71a1e14
ArchUsr64 <anshulusr@gmail.com>
2023-04-06 13:29:33 +0530

    moved from macroquad to miniquad for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use macroquad::prelude::*;
-
+#[derive(Clone, Debug)]
 struct Graph {
 	vertex: Vec<char>,
 	edges: Vec<(usize, usize)>,
@@ -51,8 +50,7 @@ impl GraphBuilder {
 	}
 }
 
-#[macroquad::main("BasicShapes")]
-async fn main() {
+fn main() {
 	let g = GraphBuilder::new()
 		.add_vertex('A')
 		.add_vertex('B')
@@ -67,40 +65,5 @@ async fn main() {
 		.add_edge('D', 'B')
 		.add_edge('F', 'C')
 		.build();
-	let positions = g
-		.vertex
-		.iter()
-		.map(|_| (rand::gen_range(0., 1.), rand::gen_range(0., 1.)))
-		.collect::<Vec<_>>();
-	loop {
-		let (width, height) = (screen_width(), screen_height());
-		let positions = positions
-			.iter()
-			.map(|value| (value.0 * width, value.1 * height))
-			.collect::<Vec<_>>();
-		g.edges.iter().for_each(|(node1, node2)| {
-			draw_line(
-				positions[*node1].0,
-				positions[*node1].1,
-				positions[*node2].0,
-				positions[*node2].1,
-				1.,
-				RED,
-			);
-		});
-		g.vertex.iter().enumerate().for_each(|(index, value)| {
-			let x = positions[index].0;
-			let y = positions[index].1;
-			let text_size = 50.;
-			draw_circle(x, y, 25., GRAY);
-			draw_text(
-				value.to_string().as_str(),
-				x - text_size / 4.,
-				y + text_size / 4.,
-				text_size,
-				WHITE,
-			);
-		});
-		next_frame().await
-	}
+	println!("{g:?}");
 }
