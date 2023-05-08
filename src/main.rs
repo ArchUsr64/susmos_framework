28679e8ca2855188a638639f94ac6029504b9187
ArchUsr64 <anshulusr@gmail.com>
2023-05-08 17:02:58 +0530

    added graph rendering

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,85 @@
-#[derive(Clone, Debug, Hash)]
+use macroquad::prelude::*;
+
+#[derive(Clone, Debug)]
 struct Graph {
 	vertex: Vec<char>,
 	edges: Vec<(usize, usize)>,
+	hash: u64,
 }
 
 impl Graph {
-	fn seed(&self) -> u64 {
-		use std::collections::hash_map::DefaultHasher;
-		use std::hash::{Hash, Hasher};
-		let mut hasher = DefaultHasher::default();
-		self.hash(&mut hasher);
-		hasher.finish()
+	fn update(&mut self) {
+		rand::srand(self.hash);
+		if is_key_pressed(KeyCode::Space) {
+			self.hash ^= rand::gen_range(f64::MIN, f64::MAX).to_bits() ^ rand::rand() as u64;
+		}
+	}
+	fn render(&self) {
+		let float_rand = || rand::gen_range(0f32, 1f32);
+		let line = |p1: (f32, f32), p2: (f32, f32)| {
+			draw_line(
+				screen_width() * p1.0,
+				screen_height() * p1.1,
+				screen_width() * p2.0,
+				screen_height() * p2.1,
+				1.,
+				WHITE,
+			);
+		};
+		let circle = |p: (f32, f32), color| {
+			draw_circle(screen_width() * p.0, screen_height() * p.1, 50., color);
+		};
+		let character = |char, pos: (f32, f32)| {
+			draw_text(
+				format!("{char}").as_str(),
+				pos.0 * screen_width(),
+				pos.1 * screen_height(),
+				30.,
+				WHITE,
+			)
+		};
+		let pos = self
+			.vertex
+			.iter()
+			.map(|_| (float_rand(), float_rand()))
+			.collect::<Vec<_>>();
+		let color = self
+			.vertex
+			.iter()
+			.map(|_| Color {
+				r: float_rand(),
+				g: float_rand(),
+				b: float_rand(),
+				a: 1.,
+			})
+			.collect::<Vec<_>>();
+		self.edges
+			.iter()
+			.for_each(|vert_index| line(pos[vert_index.0], pos[vert_index.1]));
+		pos.iter().zip(color).for_each(|(pos, color)| {
+			circle(*pos, color);
+		});
+		pos.iter().enumerate().for_each(|(index, pos)| {
+			character(self.vertex[index], *pos);
+		});
 	}
 }
 
+#[derive(Hash)]
 struct GraphBuilder {
-	graph: Graph,
+	vertex: Vec<char>,
+	edges: Vec<(usize, usize)>,
 }
 impl GraphBuilder {
 	pub fn new() -> Self {
 		Self {
-			graph: Graph {
-				vertex: Vec::new(),
-				edges: Vec::new(),
-			},
+			vertex: Vec::new(),
+			edges: Vec::new(),
 		}
 	}
 	pub fn add_vertex(mut self, value: char) -> Self {
-		if !self.graph.vertex.contains(&value) {
-			self.graph.vertex.push(value);
+		if !self.vertex.contains(&value) {
+			self.vertex.push(value);
 		}
 		self
 	}
@@ -36,8 +87,7 @@ impl GraphBuilder {
 		self = self.add_vertex(value1);
 		self = self.add_vertex(value2);
 		let find_index = |value| {
-			self.graph
-				.vertex
+			self.vertex
 				.iter()
 				.enumerate()
 				.find(|(_, char)| **char == value)
@@ -47,21 +97,29 @@ impl GraphBuilder {
 		let index1 = find_index(value1);
 		let index2 = find_index(value2);
 		println!("{index1}, {index2}");
-		if !(self.graph.edges.contains(&(index1, index2))
-			|| self.graph.edges.contains(&(index2, index1)))
-		{
+		if !(self.edges.contains(&(index1, index2)) || self.edges.contains(&(index2, index1))) {
 			println!("Added: {value1}, {value2}");
-			self.graph.edges.push((index1, index2))
+			self.edges.push((index1, index2))
 		}
 		self
 	}
 	pub fn build(self) -> Graph {
-		self.graph
+		use std::collections::hash_map::DefaultHasher;
+		use std::hash::{Hash, Hasher};
+		let mut hasher = DefaultHasher::default();
+		self.hash(&mut hasher);
+		let hash = hasher.finish();
+		Graph {
+			vertex: self.vertex,
+			edges: self.edges,
+			hash,
+		}
 	}
 }
 
-fn main() {
-	let g = GraphBuilder::new()
+#[macroquad::main("susmos")]
+async fn main() {
+	let mut g = GraphBuilder::new()
 		.add_vertex('A')
 		.add_vertex('B')
 		.add_vertex('C')
@@ -76,4 +134,10 @@ fn main() {
 		.add_edge('F', 'C')
 		.build();
 	println!("{g:?}");
+	loop {
+		clear_background(BLACK);
+		g.update();
+		g.render();
+		next_frame().await
+	}
 }
